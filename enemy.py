69e2fb8b58877a6a95ead4abdc95216b29d6f029
isidars <54060555+isidars@users.noreply.github.com>
2025-04-10 17:03:46 +0200

    Update enemy.py
    Creation de l'ennemie souris

--- enemy.py
+++ enemy.py
@@ -1,1 +1,25 @@
+class Enemy:
+    def __init__(self, name, stats):
+        self.name = name
+        self.stats = stats
+        self.hp = stats["vigueur"]
+        self.mp = stats["magie"]
 
+    def afficher_stats(self):
+        print(f"\nStatistiques de {self.name} :")
+        for stat, value in self.stats.items():
+            print(f"- {stat.capitalize()} : {value}")
+        print(f"PV : {self.hp} / {self.stats['vigueur']}")
+        print(f"PM : {self.mp} / {self.stats['magie']}\n")
+
+# Fonction pour créer la souris
+def create_mouse():
+    stats = {
+        "force": 50,
+        "chance": 40,
+        "vigueur": 100,
+        "vitesse": 110,
+        "defense": 30,
+        "magie": 0
+    }
+    return Enemy("Souris sauvage", stats)

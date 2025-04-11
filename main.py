ef98e7a71cdfd11fc05c899d1f32be5288208b5e
isidars <54060555+isidars@users.noreply.github.com>
2025-04-11 09:05:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from player import Player
+from enemy import create_mouse
+from combat import effectuer_combat
+
+# Création des personnages
+joueur = Player("Héros")  # classe de base avec stats = 100 partout
+souris = create_mouse()
+
+# Affichage
+joueur.afficher_stats()
+souris.afficher_stats()
+
+# Combat !
+effectuer_combat(joueur, souris)
 
